fe86175c51a0e2dfc9ea69d5006c8e45506a5826
ShantanuTiwari06 <shantanut710@gmail.com>
2020-11-06 21:23:44 +0530

    Add Comments to understand for anyone.

--- reverseString.c
+++ reverseString.c
@@ -1,20 +1,21 @@
+// Program to reverse a sting without strrev().
+
 #include <stdio.h>
 
 int main()
 {
-    char s[50] = "Stan";
-    char ch;
-    int l,i;
+    char s[50], ch;             // ch for swapping & s[50] to store string.
+    int l,i;                    // l for length & i for loop.
     
     printf("Enter String ");
-    // gets(s);
+    gets(s);                     
     
-    for(l=0;s[l];l++);
+    for(l=0;s[l];l++);          // Find the length of string.
     
-    for(i=0;i<l/2;i++) {
+    for(i=0;i<l/2;i++) {        // Loop & Logic for swapping.
         ch = s[i];
         s[i] = s[l-1-i];
         s[l-1-i] = ch;
     }
-    printf("Reverse is %s:",s);
+    printf("Reverse is %s:",s);    // Print the output i.e.; Reverse String.
 }
