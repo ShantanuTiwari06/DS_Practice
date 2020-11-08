bc7bf83267d837cc27881a2f2f342b533c1ea793
ShantanuTiwari06 <shantanut710@gmail.com>
2020-11-08 11:36:05 +0530

    Insert_at_end_function

--- Link_List.c
+++ Link_List.c
@@ -15,3 +15,24 @@ struct node* createNode()                           // function to create new-no
     n=(struct node *)malloc(sizeof(struct node));    // assign whatever is return by the malloc function into n & type cast it in node bcoz malloc() return void type pointer.
     return(n);
 }
+
+// Insert Node at end Function
+
+void insertNode()                      // function to enter values in node.
+{
+    struct node * temp, *t;            // *temp used, when we enter 1st node in list otherwise in *t.
+    temp = createNode();               // Here, (n) is destroyed of createNode().
+    printf("Enter a number ");          // message to user to take value for node.
+    scanf("%d",&temp->info);           // store value in (info) variable of node & pass it to temp.
+    temp->link = NULL;                 // Set Link i.e.; pointer to NULL.
+
+    if (START == NULL)                 // Condition to check node is 1st node or not, If 1st otherwise in else.
+        START = temp;                  // Store temp info into START pointer.  
+    else
+    {
+        t = START;                    // Store whatever value in START pass into (t) & continue this process till NULL.
+        while (t->link!=NULL)         // Continue till link pointer to NULL. 
+            t = t->link;
+        t->link = temp;             // store temp pointer value into t->link.
+    }   
+}
