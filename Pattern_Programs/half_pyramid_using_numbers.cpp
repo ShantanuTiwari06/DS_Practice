471c638f662ef46f58e73842c650d2c403eb3cbf
ShantanuTiwari06 <shantanut710@gmail.com>
2020-11-10 14:41:27 +0530

    pattern_program

--- Pattern_Programs/half_pyramid_using_numbers.cpp
+++ Pattern_Programs/half_pyramid_using_numbers.cpp
@@ -9,3 +9,23 @@
 1 2 3 4 5 
 
 ********************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i, j, rows;
+	cout << "Enter the number of rows. ";
+	cin >> rows;
+
+	for (i = 1; i <= rows; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			cout << j << " ";
+		}
+		cout << endl;
+	}
+	return 0; 
+}
