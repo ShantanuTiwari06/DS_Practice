9d024fdbf145b9567640a7addef1999609ccbad0
ShantanuTiwari06 <shantanut710@gmail.com>
2020-11-11 22:56:07 +0530

    init

---
+++ reverse_an_array.cpp
@@ -0,0 +1,22 @@
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n, i;
+    int arr[n];
+    
+    cout << "Enter size of array: " << endl;                 
+    cin >> n;
+    
+    cout << "Enter array elements: " << endl;
+    for(i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    cout << "Reversed array is: " << endl;
+    for(i = n-1; i >= 0; i--) {                            
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
