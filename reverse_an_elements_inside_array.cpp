e391b55f419e3b84281b77290edb8ddb057a9ae7
ShantanuTiwari06 <shantanut710@gmail.com>
2020-11-12 23:36:47 +0530

    init

---
+++ reverse_an_elements_inside_array.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream> 
+using namespace std; 
+  
+
+int reversDigits(int num) { 
+    int rev_num = 0; 
+    while (num > 0) { 
+        rev_num = rev_num * 10 + num % 10; 
+        num = num / 10; 
+    } 
+    return rev_num; 
+} 
+
+int main() {
+    int arr[7];
+
+    cout << "Enter 2 numbers: " << endl;
+
+
+    for (int i = 0; i < 2; ++i) {
+        cin >> arr[i];
+    }
+
+
+    cout << "The numbers are: ";
+    for (int i = 0; i < 2; ++i) {
+        cout << reversDigits(arr[i]) << "  ";
+    }
+    
+}
