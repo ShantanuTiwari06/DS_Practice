64df27cf05c01403d561ebcb3b6ab2e6331b9434
ShantanuTiwari06 <shantanut710@gmail.com>
2020-11-12 23:38:40 +0530

    adding_comments

--- reverse_an_elements_inside_array.cpp
+++ reverse_an_elements_inside_array.cpp
@@ -2,7 +2,7 @@
 #include <iostream> 
 using namespace std; 
   
-
+// Function to return the reverse of n.
 int reversDigits(int num) { 
     int rev_num = 0; 
     while (num > 0) { 
@@ -15,14 +15,13 @@ int reversDigits(int num) {
 int main() {
     int arr[7];
 
+    //  store input from user to array
     cout << "Enter 2 numbers: " << endl;
-
-
     for (int i = 0; i < 2; ++i) {
         cin >> arr[i];
     }
 
-
+    //  print array elements
     cout << "The numbers are: ";
     for (int i = 0; i < 2; ++i) {
         cout << reversDigits(arr[i]) << "  ";
