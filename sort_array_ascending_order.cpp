e299c415f47eda78cd080a88464eaad8e9247e77
ShantanuTiwari06 <shantanut710@gmail.com>
2020-11-13 22:51:31 +0530

    adding_comments

--- sort_array_ascending_order.cpp
+++ sort_array_ascending_order.cpp
@@ -5,22 +5,25 @@ int main()
 {
     int i, j, n, temp;
     int arr[n];
- 
+
+    // Enter the size of array
     cout << "Enter size of array: " << endl;
     cin >> n;
- 
+     
+    // Storing elements of array
     cout << "Enter elements in array: " << endl;
     for(i = 0 ; i < n ; i++)
     {
         cin >> arr[i];
     }
 
-
+    //Sorting an array in ascending order
     for(i = 0 ; i < n ; i++)
     {
         for(j = i + 1 ; j < n ; j++)
         {
-
+            
+            //If there is a smaller element found on right of the array then swap it.
             if(arr[j] < arr[i])
             {
                 temp = arr[i];
@@ -30,6 +33,7 @@ int main()
         }
     }
 
+    // Printing the sorted array in ascending order
     cout << "Elements of array in sorted ascending order: " << endl;
     for(i = 0 ; i < n; i++)
     {
