77ff6b5dfbc78cf8afadee1ef9da69bf1018c6c8
ShantanuTiwari06 <shantanut710@gmail.com>
2020-11-15 22:41:21 +0530

    adding_comments

--- Pattern_Programs/inverted_half_pyramid_of_*.c
+++ Pattern_Programs/inverted_half_pyramid_of_*.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 int main() {
-   int i, j, rows;
+
+   int i, j, rows;                          // (i) for row, (j) for column, (rows) for user input.
+   
+   // User input.
    printf("Enter the number of rows: ");
    scanf("%d", &rows);
-   for (i = rows; i >= 1; --i) {
+
+   // loop for row.
+   for (i = rows; i >= 1; --i) {   
+       // loop for column.
       for (j = 1; j <= i; ++j) {
          printf("* ");
       }
-      printf("\n");
+      printf("\n");                           // for new line.
    }
    return 0;
 }
