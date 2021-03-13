757e1184341343274d4be88e2fe7e764ed9c8da1
Shantanu <shantanut710@gmail.com>
2021-03-13 23:03:40 +0530

    in

---
+++ shuffleTheArray.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+// ---------------------------------- C++ ---------------------------------------------
+//class Solution {
+//    public int[] shuffle(int[] nums, int n) {
+//        int sample[] = new int[nums.length];
+//
+//        for(int i = 0; i < n; i++) {
+//            sample[2*i] = nums[i];
+//            sample[2*i + 1] = nums[i+n];
+//        }
+//        return sample;
+//    }
+//}
+// ---------------------------------- C++ ---------------------------------------------
+
+// Explaination :-----
+
+// Input Array = [1,2,3,4,5,6]         Target = n       // n = 3
+
+// position = 0 , position + n = 3           ->  1 , 4
+// position = 1 , position + n = 4           ->  1 , 4 , 2 , 5
+// position = 2 , position + n = 5           ->  1 , 4 , 2 , 5 , 3 , 6
+// position = 3 , position + n = 6           ->  1 , 4 , 2 , 5 , 3 , 6
+
+// ---------------------------------- Time Taken : 48 ms ---------------------------------------------
+class Solution {
+    var result = [Int]()
+    var position = 0
+    
+    func shuffle(_ nums: [Int], _ n: Int) -> [Int] {
+        nums.forEach { _ in
+            if position <= n-1 {
+                result.append(nums[position])
+                result.append(nums[position + n])
+            }
+            position += 1
+        }
+        print(result)
+        return result
+    }
+}
+Solution().shuffle([1,1,2,2],  2)
