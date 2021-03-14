f518f352122ad057a3ffb95f1396be856b48574e
Shantanu <shantanut710@gmail.com>
2021-03-14 23:13:27 +0530

    36 ms

--- shuffleTheArray.swift
+++ shuffleTheArray.swift
@@ -61,3 +61,16 @@ class Solution {
         return out
     }
 }
+
+// ---------------------------------- Time Taken : 36 ms ---------------------------------------------
+
+class Solution {
+    func shuffle(_ nums: [Int], _ n: Int) -> [Int] {
+        var res = [Int]()
+        for i in 0..<n {
+            res.append(nums[i])
+            res.append(nums[i + n])
+        }
+        return res
+    }
+}
