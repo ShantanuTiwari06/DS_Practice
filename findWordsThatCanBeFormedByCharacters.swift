eb321525b797d312b237221b3115feb5243a7c5f
Shantanu <shantanut710@gmail.com>
2021-03-23 10:51:40 +0530

    explanation

---
+++ findWordsThatCanBeFormedByCharacters.swift
@@ -0,0 +1,14 @@
+// ----------------------------------- Explanation -----------------------------------
+/*
+Example 1:
+Input: words = ["cat","bt","hat","tree"], chars = "atach"
+Output: 6
+Explanation:
+The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
+
+Example 2:
+Input: words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+Output: 10
+Explanation:
+The strings that can be formed are "hello" and "world" so the answer is 5 + 5 = 10.
+*/
