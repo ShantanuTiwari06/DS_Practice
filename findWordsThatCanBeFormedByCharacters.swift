0d64a2de56a022f8cd9d6222466432164bc23b8a
Shantanu <shantanut710@gmail.com>
2021-03-23 10:52:15 +0530

    172 ms

--- findWordsThatCanBeFormedByCharacters.swift
+++ findWordsThatCanBeFormedByCharacters.swift
@@ -12,3 +12,43 @@ Output: 10
 Explanation:
 The strings that can be formed are "hello" and "world" so the answer is 5 + 5 = 10.
 */
+
+// ----------------------------------------- Time Taken : 172 ms ---------------------------------------------
+class Solution {
+    func countCharacters(_ words: [String], _ chars: String) -> Int {
+        var available = [Character: Int]()
+        
+        for character in chars {
+            available[character, default: 0] += 1
+//            print("This is available \(available)")
+        }
+        
+        var solution = 0
+        
+        for word in words {
+//            print(word)
+            var options = available
+            var valid = true
+            for character in word {
+//                print("This is character \(character)")
+//                print("This is options[character] 1 : \(String(describing: options[character] ?? 0))")
+                if let option = options[character], option > 0 {
+                    options[character] = option - 1
+//                    print("This is options[character] 2 : \(String(describing: options[character] ?? 0))")
+                } else {
+//                    print("elseOne")
+                    valid = false
+                    break
+                }
+            }
+            
+            if valid {
+                solution += word.count
+            }
+        }
+        
+        return solution
+    }
+}
+print(Solution().countCharacters(["cat","bt","hat","tree", "caaat"], "atach"))
+
