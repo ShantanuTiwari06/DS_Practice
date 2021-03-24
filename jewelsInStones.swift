743e578b9ddb66a7cdabeb27deecc3423f67a214
Shantanu <shantanut710@gmail.com>
2021-03-24 13:35:43 +0530

    Time Taken - 4 ms

--- jewelsInStones.swift
+++ jewelsInStones.swift
@@ -11,4 +11,34 @@
 
  */
 
- 
+
+ // ------------------------------- Time Taken - 4 ms ; Memory - 14.1 MB ------------------------
+class Solution {
+
+    func numJewelsInStones(_ jewels: String, _ stones: String) -> Int {
+        var available = [Character: Int]()
+
+        for character in stones {
+            available[character, default: 0] += 1
+//            print("This is available \(available)")
+        }
+        
+        var solution = 0
+        let options = available
+        
+        for word in jewels {
+//             print(word)
+//            print("This is options[character] 1 : \(String(describing: options[word] ?? 0))")
+            if options[word] != nil {
+                if options[word] == 0 {
+                    solution = 0
+                }else{
+                    solution +=  options[word]!
+                }
+            }
+        }
+        return solution
+    }
+}
+
+print(Solution().numJewelsInStones("z", "ZZ"))
