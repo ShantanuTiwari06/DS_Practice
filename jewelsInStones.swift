b6f599dcb32f236417d2a50bab412b7df313817a
Shantanu <shantanut710@gmail.com>
2021-03-24 13:34:54 +0530

    explanation

---
+++ jewelsInStones.swift
@@ -0,0 +1,14 @@
+/*
+---------------------------- EXPLANATION ------------------------
+ 
+ 1.
+ Input: jewels = "aA", stones = "aAAbbbb"
+ Output: 3
+ 
+ 2.
+ Input: jewels = "z", stones = "ZZ"
+ Output: 0
+
+ */
+
+ 
