fa92336094e75d5b5adf36cf6beb9bcf95420568
Shantanu <shantanut710@gmail.com>
2021-03-26 13:37:49 +0530

    Time Taken : Runtime: 236 ms

--- addToArray-FormOfInteger.swift
+++ addToArray-FormOfInteger.swift
@@ -21,3 +21,38 @@ Explanation: 1200 + 34 = 1234
  Explanation: 9999999999 + 1 = 10000000000
  ------------------------------------------------------------------------------------------------
  */
+
+
+// ----------------------------- Time Taken : Runtime: 236 ms ; Memory Usage: 15 MB  ---------------------------
+
+class Solution {
+
+func addToArrayForm(_ A: [Int], _ K: Int) -> [Int] {
+        var answer: [Int] = []
+        var carry = K
+        var i = A.count - 1
+//        print(i)
+    
+        // i =  3, 2, 1, 0 ; carry = 12, 1, 0, 0
+        while i >= 0 || carry > 0 {
+            // ternary operator.
+            // ( Value = Condition ? valueIfTrue : valueIfFalse )
+            let aDigit = i >= 0 ? A[i] : 0
+//            print("This is aDigit \(aDigit)") // 4, 3, 2, 1
+            // sum = 12 + 4
+            let sum = carry + aDigit // 12+4, 1+3, 0+2, 0+1
+//            print("This is sum \(sum)") // 16 , 4, 2, 1
+            answer.append(sum % 10) // 16 % 10, 4 % 10, 2 % 10, 1 % 10
+//            print("This is answer \(answer)") // [6], [6,4], [6,4,2], [6,4,2,1]
+            
+            carry = sum / 10 // 12, 1, 0, 0, 0
+//            print("This is carry \(carry)")
+            i -= 1 //  3, 2, 1, 0, -1
+//            print("This is i \(i)")
+//            print("-----------------------------")
+        }
+    
+        return answer.reversed() // [1, 2, 4, 6]
+    }
+}
+print(Solution().addToArrayForm([1,2,3,4], 12))
