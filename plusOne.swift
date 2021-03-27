0bec57860362253a90750ca8614db146aa36b78c
Shantanu <shantanut710@gmail.com>
2021-03-27 23:48:06 +0530

    explanation

---
+++ plusOne.swift
@@ -0,0 +1,18 @@
+/*
+ Example 1:
+ Input: digits = [1,2,3]
+ Output: [1,2,4]
+ Explanation: The array represents the integer 123.
+
+ Example 2:
+ Input: digits = [4,3,2,1]
+ Output: [4,3,2,2]
+ Explanation: The array represents the integer 4321.
+
+ Example 3:
+ Input: digits = [0]
+ Output: [1]
+
+ 1 <= digits.length <= 100
+ 0 <= digits[i] <= 9
+ */
