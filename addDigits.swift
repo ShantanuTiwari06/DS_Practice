d1554d00efcbf0c761a778e77cb67a15326fc091
Shantanu <shantanut710@gmail.com>
2021-03-31 11:30:04 +0530

    Time taken : 12 ms

--- addDigits.swift
+++ addDigits.swift
@@ -13,3 +13,33 @@
  Output: 0
  
  */
+// ---------------------------- Time taken : 12 ms  ----------------------------
+class Solution {
+
+    
+    func addDigits(_ num: Int) -> Int {
+//      print(num) // 4987, 28,
+        
+        if num < 10 {
+            return num // 1
+        }
+        
+        var sumOfDigits = 0
+        var temp = num // 4987 , 28 , 10
+        
+        // loop
+        while temp > 0 {
+            // 1st - 0+7= 7, 7+8 = 15, 15+9=24, 24+4 = 28
+            // 2nd - 0+8= 8, 8+2 = 10
+            // 3rd - 0+0= 0, 0+1 =  1
+            sumOfDigits = sumOfDigits + temp % 10
+            // 1st -  4987/10 = 498,  49,  4,  0
+            // 2nd -  28/10   =   2,   0
+            // 3rd -  10/10   =   1,   0
+            temp = temp / 10
+        }
+        
+        return addDigits(sumOfDigits) // 28, 10 , 1
+    }
+}
+print(Solution().addDigits(4987))
