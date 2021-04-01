ef9909f7d7ac34f608dd770561449e667fbae0aa
Shantanu <shantanut710@gmail.com>
2021-04-01 10:31:56 +0530

    EXPLANATION

---
+++ addBinary.swift
@@ -0,0 +1,20 @@
+/*
+ // --------------------------------- EXPLANATION ---------------------------------
+
+Given two binary strings a and b, return their sum as a binary string.
+
+Example 1:
+Input: a = "11", b = "1"
+Output: "100"
+
+Example 2:
+Input: a = "1010", b = "1011"
+Output: "10101"
+ 
+ Therefore in binary:
+ 0 + 0 = 0
+ 0 + 1 = 1
+ 1 + 0 = 1
+ 1 + 1 = 10 (which is 0 carry 1)
+ 
+*/
