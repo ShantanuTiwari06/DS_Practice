354534ce45e8be712ce5ea17395e9fd77c89e6c0
Shantanu <shantanut710@gmail.com>
2021-04-01 10:32:25 +0530

    Time Taken : 36 ms ; Memory : 14.4 MB

--- addBinary.swift
+++ addBinary.swift
@@ -18,3 +18,71 @@ Output: "10101"
  1 + 1 = 10 (which is 0 carry 1)
  
 */
+
+// --------------------------------- Time Taken : 36 ms ; Memory : 14.4 MB ---------------------------------
+class Solution {
+    var solution = ""
+    
+    func addBinary(_ a: String, _ b: String) -> String {
+//        if a == "0"
+        var firstNoLength = a.count
+        var secondNoLength = b.count
+        var firstNo = a
+        var secondNo = b
+        var carry = 0
+        var sum = 0
+
+        while firstNoLength > 0 || secondNoLength > 0 {
+//                print(firstNo)
+            var lastDigitOfFirstNo = firstNo.suffix(1)
+            var lastDigitOfSecondNo = secondNo.suffix(1)
+            firstNo = String(firstNo.dropLast())
+            secondNo = String(secondNo.dropLast())
+//            print("This is carry 1: \(carry)")
+          
+//            print("This is 1st : \(lastDigitOfFirstNo)")
+//            print("This is 2nd : \(lastDigitOfSecondNo)")
+            
+            if lastDigitOfFirstNo == "" {
+                lastDigitOfFirstNo = "0"
+            }else if lastDigitOfSecondNo == "" {
+                lastDigitOfSecondNo = "0"
+            }
+            
+            sum = Int(lastDigitOfFirstNo)! + Int(lastDigitOfSecondNo)! + carry
+            carry = 0
+            
+            if sum == 0 {
+                carry = 0
+                solution.append("0")
+            } else if sum == 2 {
+                carry = 1
+                solution.append("0")
+//                print("This is solution 1 : \(solution)")
+            } else if sum == 3 {
+                carry = 1
+                solution.append("1")
+            } else{
+                solution.append("1")
+            }
+//            print("This is solution 2 : \(solution)")
+
+//            print("This is sum : \(sum)")
+//            print("This is carry 2: \(carry)")
+//            print("--------------------------")
+
+            firstNoLength -= 1
+            secondNoLength -= 1
+        }
+        if sum == 2 {
+            solution.append("1")
+        }else if sum == 3 {
+            solution.append("1")
+        }
+        
+        
+        return String(solution.reversed())
+    }
+    
+}
+print(Solution().addBinary("111", "11"))
