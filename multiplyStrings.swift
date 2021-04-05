66bdfa81967756202fc2d936528500b97b7700d7
Shantanu <shantanut710@gmail.com>
2021-04-05 15:30:56 +0530

    EXPLNATION

---
+++ multiplyStrings.swift
@@ -0,0 +1,20 @@
+
+/*
+ ---------------------------- EXPLNATION  ----------------------------
+ Example 1:
+ Input: num1 = "2", num2 = "3"
+ Output: "6"
+
+ Example 2:
+ Input: num1 = "123", num2 = "456"
+ Output: "56088"
+
+ */
+// ------------ Power ------------
+//var a = 10
+//var b = 5
+//var c = pow(Double(a), Double(b))
+//var solutionInt = Int(c)
+//print(solutionInt)
+
+/*
