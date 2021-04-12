750dfd7077668fb218d394210957cf2d0ccef665
Shantanu <shantanut710@gmail.com>
2021-04-12 11:43:18 +0530

    EXPLANTION

---
+++ checkIfItIsAStraightLine.swift
@@ -0,0 +1,22 @@
+/*
+ -------------- EXPLANTION --------------
+ You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
+
+ Example 1:
+ Input: coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+ Output: true
+
+ Example 2:
+ Input: coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+ Output: false
+
+ 
+ NOTE : Slope of every two points is same .
+ // Slope of line formed by
+ // two points (y2, y1), (x2, x1)
+
+ Slope of Line = y2 - y1
+                ---------
+                 x2 - x1
+ 
+ */
