0060a278e4ae25f70299b1812eb280292236413f
Shantanu <shantanut710@gmail.com>
2021-04-12 11:43:39 +0530

    Time Taken : 40 ms ; Memory : 14 MB

--- checkIfItIsAStraightLine.swift
+++ checkIfItIsAStraightLine.swift
@@ -20,3 +20,35 @@
                  x2 - x1
  
  */
+
+// ---------------------------------- Time Taken : 40 ms ; Memory : 14 MB ----------------------------------
+class Solution {
+    
+    func checkStraightLine(_ coordinates: [[Int]]) -> Bool {
+        
+        var position = 2
+        
+        for _ in coordinates {
+            var firstCoordinates = [Int]()
+            var secondCoordinates = [Int]()
+            var thirdCoordinates = [Int]()
+
+            if position < coordinates.count {
+                firstCoordinates = coordinates[position-2]
+                secondCoordinates = coordinates[position-1]
+                thirdCoordinates = coordinates[position]
+
+                position += 1
+               
+                let collinearity = firstCoordinates[0] * (secondCoordinates[1] - thirdCoordinates[1]) + secondCoordinates[0] * (thirdCoordinates[1] - firstCoordinates[1]) + thirdCoordinates[0] * (firstCoordinates[1] - secondCoordinates[1])
+
+                if collinearity != 0 {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+}
+
+print(Solution().checkStraightLine([[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]))
