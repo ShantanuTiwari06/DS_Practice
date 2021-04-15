2375fc0cd7bdb67896ef53cb1e5ff333798e301a
Shantanu <shantanut710@gmail.com>
2021-04-15 19:05:47 +0530

    EXPLANATION

---
+++ checkIfA-WordOccursAs-A-PrefixOfAnyWordInASentence.swift
@@ -0,0 +1,28 @@
+/*
+ ------------------------ EXPLANATION  ------------------------
+ 
+ Example 1:
+ Input: sentence = "i love eating burger", searchWord = "burg"
+ Output: 4
+ Explanation: "burg" is prefix of "burger" which is the 4th word in the sentence.
+
+ Example 2:
+ Input: sentence = "this problem is an easy problem", searchWord = "pro"
+ Output: 2
+ Explanation: "pro" is prefix of "problem" which is the 2nd and the 6th word in the sentence, but we return 2 as it's the minimal index.
+
+ Example 3:
+ Input: sentence = "i am tired", searchWord = "you"
+ Output: -1
+ Explanation: "you" is not a prefix of any word in the sentence.
+
+ Example 4:
+ Input: sentence = "i use triple pillow", searchWord = "pill"
+ Output: 4
+
+ Example 5:
+
+ Input: sentence = "hello from the other side", searchWord = "they"
+ Output: -1
+
+ */
