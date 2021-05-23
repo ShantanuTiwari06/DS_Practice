048ff6b20540db99c610c94bed7bc02116351fb2
Shantanu <shantanut710@gmail.com>
2021-05-23 17:44:24 +0530

    Aproach 1 : Without Sorting / In a Single Traversal.

--- find2ndLargestNoInArr.cpp
+++ find2ndLargestNoInArr.cpp
@@ -1,4 +1,21 @@
 
+/**
+Aproach 1 : Without Sorting / In a Single Traversal.  
+
+Step 1 : Take 3 var i, first, second and set first and second = INT_MIN
+Step 2 : Check condition if arr_size < 2 Then, print "Invalid Input" & after print use return keyword.
+Step 3 : Run Loop from i = 0 to less than arr_size 
+Step 4 : Inside loop 1st condition is : if (arr[i] > first) , Then, s = f ; f = arr[i];
+Step 5 : Inside loop 2nd condition is : if (arr[i] > s && arr[i] != f) ; Then, s =  arr[i];
+Step 6 : Outside loop if (s == Int_Min) ; Then, print "None 2nd largest no found."
+            Else, print "2nd Largest no is :" << s;
+
+NOTE :  1. make func with return type void
+        2. in main() at last return 0;
+
+**/
+
+
 #include <iostream>
 #include <limits.h>
 
